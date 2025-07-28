c11a0ed7c8f110e697823307e0996fdbb4a36c83
Arthav Jain <ytamaze67@gmail.com>
2025-07-28 18:37:14 +0530

    Added Gradient Generator

--- index.js
+++ index.js
@@ -139,7 +139,8 @@ function fillTable() {
         ["Day 28", "Target Reflex Test", "public/Day-28_Target_Reflex_Test/index.html"],
         ["Day 29", "Snake And Ladder Game", "public/Snake-and-Ladder-Game/index.html"],
         ["Day 30", "Note Taker", "public/Day-42_NoteTaker/index.html"],
-         ["Day 31","Rock Paper Scissor Game" ,"public/Day-23_RockPaperScissor/index.html" ]
+         ["Day 31","Rock Paper Scissor Game" ,"public/Day-23_RockPaperScissor/index.html"],
+        ["Day 32", "Gradient Generator", "public/Gradient_Generator/index.html"],
     ];
 
 
