2880924fc7b8fbd246b9e95844387d2d486325fd
Reeteesha Roy <reeteesharoy@gmail.com>
2025-07-29 17:34:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ function fillTable() {
         ["Day 28", "Target Reflex Test", "public/Day-28_Target_Reflex_Test/index.html"],
         ["Day 29", "Snake And Ladder Game", "public/Snake-and-Ladder-Game/index.html"],
         ["Day 30", "Note Taker", "public/Day-42_NoteTaker/index.html"],
+        ["Day 31", "Word Guess Game, ""],
     ];
 
 
