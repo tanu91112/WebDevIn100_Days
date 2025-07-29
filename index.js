9e866f37642bc574b6731d82bec26731c76e3b19
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-07-29 14:33:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function fillTable() {
         ["Day 34"," Periodic Table", "https://learn-periodic-table.netlify.app/"],
         ["Day 35"," Book Recommendation", "https://book-recomendation.netlify.app/"],
         ["Day36","World Clock","public/World_Clock/index.html"],
-      ["Day 37", "Space War Game" ,"./public/Space-War-Game/index.html"],
+      ["Day 40", "Space War Game" ,"./public/Space-War-Game/index.html"],
     ];
 
 
