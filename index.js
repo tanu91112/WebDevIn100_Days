5eb4f69bc7d46ab2852f94a2168a533c77551f4b
Tanu Chandravanshi <134772713+tanu91112@users.noreply.github.com>
2025-07-30 20:08:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ function fillTable() {
           ["Day 38", "Gradient Generator", "public/Gradient_Generator/index.html"],
           ["Day 40", "Space War Game", "./public/Space-War-Game/index.html"],
           ["Day 44", "Notes App", "./public/Day-36_Notes_App/index.html"],
+          ["Day 46", "Mood Based Music Suggester", "./public/Mood_Music_Suggester/index.html"],
           ["Day 47", "Pomodoro App", "/public/Day-47_Pomodoro-app/index.html"],
           ["Day 48", "Space Jumper Game", "./public/Space-Jumper-Game/index.html"],
           ["Day 49", "CalRace", "./public/Day-45/index.html"],
